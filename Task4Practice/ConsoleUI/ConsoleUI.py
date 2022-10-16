03363c511282c8c14620ce370ddab1c3775954c1
OrengoA1587 <102257741+OrengoA1587@users.noreply.github.com>
2022-10-16 19:48:05 -0400

    Add Task4

---
+++ Task4Practice/ConsoleUI/ConsoleUI.py
@@ -0,0 +1,23 @@
+import os
+
+
+def get_path_depth(path):
+    path = os.path.normpath(path)
+   
+    return len(path.split(os.sep)) -1
+def generate_dir_report(path):
+     
+    for root,dirs,files in sorted((os.walk(path))): 
+        filepath = root
+        print(get_path_depth(filepath))
+         
+        for file_name in sorted(files):            
+             
+            print(f'{file_name}')
+         
+# TODO: Place your code here
+if __name__ == '__main__':
+     pass
+     generate_dir_report('data/dir-top')
+
+ 
