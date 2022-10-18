324a09ee41720e771fc7ecad076848851bbac9f3
OrengoA1587 <102257741+OrengoA1587@users.noreply.github.com>
2022-10-18 18:45:12 -0400

    Task4 Update

--- Task4Practice/ConsoleUI/ConsoleUI.py
+++ Task4Practice/ConsoleUI/ConsoleUI.py
@@ -5,15 +5,15 @@ def get_path_depth(path):
     path = os.path.normpath(path)
      
     return len(path.split(os.sep))
-def generate_dir_report(path,out_file,show_files,num_files,file_size,h1):
+def generate_dir_report(path,out_file,show_files,num_files,file_size,hl):
     with open(out_file,mode='w') as wf:
+        main_path_depth = get_path_depth(path)
         for root, dirs, files in os.walk(path):
-            #size = files
-        
-            level = root.replace(path, '').count(os.sep)
+            dir_indent =  get_path_depth(root) - main_path_depth - 1            
+            file_indent = dir_indent + 1             
+            level = file_indent         
             if num_files == True:
-                num_f = f'({len(files)} files)'
-        
+                num_f = f'({len(files)} files)'        
             if level == 0:
                  indent = '' * 2 * (level)
                  print('+{}{}'.format(indent, os.path.basename(root)), num_f, file = wf)                   
@@ -25,7 +25,7 @@ def generate_dir_report(path,out_file,show_files,num_files,file_size,h1):
                  indent = '' * 3 * (level)
                  print('  |-+ {}{}'.format(indent, os.path.basename(root)),num_f, file = wf)
              
-            subindent = ' ' * 2 * (level)
+            file_indent = ' ' * 2 * (level)
             if show_files == True:             
                 for f in files:  
                     if file_size == True:
@@ -34,14 +34,15 @@ def generate_dir_report(path,out_file,show_files,num_files,file_size,h1):
                         size = f'({size} bytes)' 
                     else:
                         size = ''
-                    if f.endswith(f'.{h1}'):
-                        print('{}{}'.format(subindent + '|-- ', f),size, '<--', file = wf)
+                    if f.endswith(f'.{hl}'):
+                        print('{}{}'.format(file_indent + '|-- ', f),size, '<--', file = wf)
                     else:
-                        print('{}{}'.format(subindent + '|-- ', f),size, file = wf)
+                        print('{}{}'.format(file_indent + '|-- ', f),size, file = wf)
+        
     wf.close()     
 # TODO: Place your code here
 if __name__ == '__main__':
      pass
-     generate_dir_report('data/dir-top','dir-report.txt', show_files=True,num_files=True,file_size=True,h1='txt')
+     generate_dir_report('data/dir-top','dir-report.txt', show_files=True,num_files=True,file_size=True,hl='txt')
 
  
